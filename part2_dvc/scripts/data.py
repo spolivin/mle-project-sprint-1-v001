97d469e4abc4e91b4e0660b7b11ebcc90f2ec1de
Sergey Polivin <s.polivin@gmail.com>
2024-06-10 17:17:14 +0000

    Initial commit for Stage 2

---
+++ part2_dvc/scripts/data.py
@@ -0,0 +1,50 @@
+import os
+import yaml
+
+import pandas as pd
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+
+
+def create_connection():
+    """Connects to PostreSQL database."""
+
+    # Loading envirornmental variables
+    load_dotenv()
+
+    # Retrieving the values of env-variables
+    host = os.environ.get('DB_DESTINATION_HOST')
+    port = os.environ.get('DB_DESTINATION_PORT')
+    db = os.environ.get('DB_DESTINATION_NAME')
+    username = os.environ.get('DB_DESTINATION_USER')
+    password = os.environ.get('DB_DESTINATION_PASSWORD')
+    
+    # Establishing a connection
+    db_conn = create_engine(
+        f'postgresql://{username}:{password}@{host}:{port}/{db}', 
+        connect_args={'sslmode':'require'},
+    )
+    
+    return db_conn
+
+
+def get_data():
+    """Saves the cleaned data from the DB in the local dir."""
+
+    # Reading parameters
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+    
+    # Pulling the data from DB
+    db_conn = create_connection()
+    data = pd.read_sql('SELECT * FROM flats_clean', db_conn, index_col=params['index_col'])
+    data.drop(params['cols_to_drop'], axis=1, inplace=True)
+    db_conn.dispose()
+
+    # Locally saving the data
+    os.makedirs('data', exist_ok=True)
+    data.to_csv('data/initial_data_flats.csv', index=None)
+
+
+if __name__ == '__main__':
+    get_data()
---
+++ part2_dvc/scripts/evaluate.py
@@ -0,0 +1,46 @@
+import joblib
+import json
+import os
+import yaml
+
+import pandas as pd
+from sklearn.model_selection import cross_validate
+
+
+def evaluate_model():
+	
+    # Reading the parameters
+    with open('params.yaml', 'r') as fd:
+        params = yaml.safe_load(fd)
+	
+    # Loading the trained model
+    with open('models/fitted_model_flats.pkl', 'rb') as fd:
+        pipeline = joblib.load(fd) 
+    
+    # Reading the data
+    data = pd.read_csv('data/initial_data_flats.csv')
+    
+    # Separating features from target
+    target = data[params['target_col']]
+    features = data.drop(params['target_col'], axis=1)
+    
+    # Launching cross-validation
+    cv_res = cross_validate(
+        pipeline,
+        features,
+        target,
+        cv=params['n_splits'],
+        n_jobs=params['n_jobs'],
+        scoring=params['metrics'],
+    )
+    for key, value in cv_res.items():
+        cv_res[key] = round(value.mean(), 3)
+	
+    # Locally saving cross-validation results
+    os.makedirs('cv_results', exist_ok=True)
+    with open('cv_results/cv_res_flats.json', 'w') as fd:
+        json.dump(cv_res, fd)
+
+
+if __name__ == '__main__':
+	evaluate_model()
